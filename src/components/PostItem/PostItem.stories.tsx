44952dd22e28a051e67bf4e358ec6a3b490cc35e
supersaasdev <95603286+supersaasdev@users.noreply.github.com>
2022-02-09 18:55:53 +0530

    Update PostItem.stories.tsx

--- src/components/PostItem/PostItem.stories.tsx
+++ src/components/PostItem/PostItem.stories.tsx
@@ -12,7 +12,7 @@ const Template: Story<PostItemProps> = (args) => <PostItem {...args} />;
 
 export const Board = Template.bind({});
 Board.args = {
-    url: 'https://lambdaapi.superopsalpha.com/sync',
+    url: '',
     postId: '6089a5ccc314133780617f52',
     currentUser: {
         email: 'iamcsk90@gmail.com',
